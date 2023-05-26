4c87b4f0a3b89febb85cb3f05c702f2e8859fdc0
Divya Goyal <77169739+Coder2601@users.noreply.github.com>
2023-05-26 13:03:39 +0530

    Add files via upload

--- frontend/src/data/ProductData.js
+++ frontend/src/data/ProductData.js
@@ -1,22 +1,26 @@
 export const products = [
     {
+        id:1,
         img:'/Images/food1.jpg',
-        price:'Rs. 500',
+        price:'100',
         name:'Meal'
     },
     {
+        id:2,
         img:'/Images/food3.jpg',
-        price:'Rs. 250',
+        price:'100',
         name:'Pizza'
     },
     {
+        id:3,
         img:'/Images/food4.jpg',
-        price:'Rs. 50',
+        price:'10',
         name:'Gulab Jamun'
     },
     {
+        id:4,
         img:'/Images/food7.jpg',
-        price:'Rs. 450',
+        price:'100',
         name:'Thali'
     }
 ]
